fde636b4908784f15ef7b76971531e021f07c4da
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2024-03-05 21:13:08 +0300

    Added functionality for searching, updating, inserting and deleting issues to the issues db context

--- YouTrackIssuesParser/IssuesDbContext.cs
+++ YouTrackIssuesParser/IssuesDbContext.cs
@@ -13,9 +13,32 @@ public class IssuesDbContext
         _collection = collection;
     }
 
-    public async Task SaveIssuesArray(IEnumerable<Issue> issues)
+    public async Task Add(Issue issue)
+    {
+        await _collection.InsertOneAsync(issue.ToBsonDocument());
+    }
+
+    public async Task AddRange(IEnumerable<Issue> issues)
     {
         var docs = issues.Select(x => x.ToBsonDocument()).ToList();
         await _collection.InsertManyAsync(docs);
+    }
+
+    public async Task<BsonDocument> FindById(string id)
+    {
+        var filter = Builders<BsonDocument>.Filter.Eq("id", id);
+        return await _collection.FindAsync(filter).Result.FirstOrDefaultAsync();
+    }
+
+    public async Task ReplaceById(Issue issue)
+    {
+        var bson = await FindById(issue.Id);
+        await _collection.ReplaceOneAsync(bson, issue.ToBsonDocument());
+    }
+
+    public async Task DeleteById(string id)
+    {
+        var bson = await FindById(id);
+        await _collection.DeleteOneAsync(bson);
     }
 }
