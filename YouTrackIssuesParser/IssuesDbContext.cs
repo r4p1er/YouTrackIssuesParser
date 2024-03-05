e2cc3eb8a2885e5002779faeb748ae4490b0f104
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2024-03-05 11:55:11 +0300

    Added MongoDB.Driver package; added doc id field to the issue model; added issues db context for quering and saving issues to the db

---
+++ YouTrackIssuesParser/IssuesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using YouTrackIssuesParser.Models;
+
+namespace YouTrackIssuesParser;
+
+public class IssuesDbContext
+{
+    private readonly IMongoCollection<BsonDocument> _collection;
+
+    public IssuesDbContext(IMongoCollection<BsonDocument> collection)
+    {
+        _collection = collection;
+    }
+
+    public async Task SaveIssuesArray(IEnumerable<Issue> issues)
+    {
+        var docs = issues.Select(x => x.ToBsonDocument()).ToList();
+        await _collection.InsertManyAsync(docs);
+    }
+}
